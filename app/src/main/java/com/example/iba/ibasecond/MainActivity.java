d6c46b6b613abebc6c07ed41389299b8d2e7613e
MohamedSherif94 <mohammed.said4368@el-eng.menofia.edu.eg>
2018-08-26 06:17:38 +0200

    Initial commit

---
+++ app/src/main/java/com/example/iba/ibasecond/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.iba.ibasecond;
+
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Handler mHandler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        }, 1000); // 1 second
+    }
+}

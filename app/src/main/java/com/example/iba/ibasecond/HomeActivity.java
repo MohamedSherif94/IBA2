4a47c253ed67e0be61e94066f4185fbc3e09e38b
MohamedSherif94 <mohammed.said4368@el-eng.menofia.edu.eg>
2018-08-27 13:39:44 +0200

    Adding social media and navigation drawer

--- app/src/main/java/com/example/iba/ibasecond/HomeActivity.java
+++ app/src/main/java/com/example/iba/ibasecond/HomeActivity.java
@@ -1,13 +1,81 @@
 package com.example.iba.ibasecond;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private String facebookUrl = "https://www.facebook.com/iba.eg.usa/";
+    private String twitterUrl = "https://twitter.com/iba_usa";
+    private String linkedinUrl = "https://eg.linkedin.com/in/iba-eg-usa-71192016a?trk=profile-badge";
+
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mNavigationView;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        mToolbar.setTitleTextColor(Color.WHITE);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mNavigationView = findViewById(R.id.nav_view);
+        mNavigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    public void openFacebookPage(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(facebookUrl));
+        startActivity(intent);
+    }
+
+    public void openTwitterPage(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(twitterUrl));
+        startActivity(intent);
+    }
+
+    public void openLinkedInPage(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(linkedinUrl));
+        startActivity(intent);
     }
 }
